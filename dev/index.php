2ce4a06d54af24391a09018c6c2f812a75524d27
Keliorw <miabrilo228@gmail.com>
2022-07-05 18:15:10 +0300

    first commit

---
+++ dev/index.php
@@ -0,0 +1,14 @@
+<?php
+    $title = 'Главная страница';
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <?php include("src/head.php"); ?>
+</head>
+<body>
+    <p>
+        Hello world!
+    </p>
+</body>
+</html> 
